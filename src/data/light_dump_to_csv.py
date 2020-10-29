f46f04980ea79d6c03511db6bfa7b6b8f0595a77
YihengYe <yiy291@ucsd.edu>
2020-10-28 18:47:18 -0700

    test

---
+++ src/data/light_dump_to_csv.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+def ld_to_csv(raw):
+    '''
+    this function take a file contains of .zip light_dump data and convert it to csvs that store information for every article
+
+    raw: the string path to open
+
+    '''
+    
+    return
