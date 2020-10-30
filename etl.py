dcd46ed0767d80c9ce5558f6db771261da59b162
YihengYe <yiy291@ucsd.edu>
2020-10-30 13:13:58 -0700

    fix zip

--- etl.py
+++ etl.py
@@ -145,14 +145,15 @@ def extracter(raw):
     if not os.path.exists(txtpath):
         os.makedirs(txtpath)
     for zipname in os.listdir(raw):
-        zipper=os.path.join(raw, zipname)
-        the_zip=zipfile.ZipFile(zipper)
-        for data in the_zip.namelist():
-            datafile=os.path.join(txtpath, data)
-            if not os.path.exists(datafile):
-                the_zip.extract(data, txtpath)
-                info='the data '+data+" has beeen store at "+txtpath
-                print(info)
+        if zipname.endswith('.zip'):
+            zipper=os.path.join(raw, zipname)
+            the_zip=zipfile.ZipFile(zipper)
+            for data in the_zip.namelist():
+                datafile=os.path.join(txtpath, data)
+                if not os.path.exists(datafile):
+                    the_zip.extract(data, txtpath)
+                    info='the data '+data+" has beeen store at "+txtpath
+                    print(info)
 
 
 
