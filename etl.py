c492aec641ca88a87b1a9645e9ad37dd89bc9aa3
YihengYe <yiy291@ucsd.edu>
2020-11-09 16:01:38 -0800

    more eng

--- etl.py
+++ etl.py
@@ -172,68 +172,97 @@ def ld_to_csv(txtpath):
         txtroute=os.path.join(txtpath, txtname)
         filename=txtname.split('.')[0]
         filedir=os.path.join(outpath, filename)
-        if not os.path.exists(filedir):
+        if not os.path.exists(filedir) and 'txt' in txtname:
             os.makedirs(filedir)
             with open(txtroute, 'r') as fh:
-                lines=fh.readlines()
                 table={'article':[],'time':[], 'revert':[], 'version':[], 'user':[]} #initialize the dataframe
-                max_row=100000
+                max_row=5000000
                 rows=0
                 count=0
                 nowat=0
-                maxline=len(lines)
                 exceed=False
-                for line in lines:
+                while True:
+                    line=fh.readline()
                     content=line.strip()
-                    if not '^^^' in content:
-                        current=content
-                        nowat +=1
-                        if exceed:
-                            count +=1
-                            csvname=filename+'_'+str(count)+'.csv'
-                            the_df=pd.DataFrame(table)
-                            cvpath=os.path.join(filedir, csvname)
-                            the_df.to_csv(cvpath, index=False)
-                            rows=0
-                            table={'article':[],'time':[], 'revert':[], 'version':[], 'user':[]}
-                            exceed=False
+                    if line.endswith('\n'):
+                        content=line.strip()
+                        if not '^^^' in content:
+                            current=content
+                            nowat +=1
+                            if exceed:
+                                count +=1
+                                csvname=filename+'_'+str(count)+'.csv'
+                                the_df=pd.DataFrame(table)
+                                cvpath=os.path.join(filedir, csvname)
+                                the_df.to_csv(cvpath, index=False)
+                                print("creat 5-million-row table", count)
+                                rows=0
+                                table={'article':[],'time':[], 'revert':[], 'version':[], 'user':[]}
+                                exceed=False
 
-                    else:
-                        if rows<max_row and nowat<maxline:
-                            unit=content.split(' ')
-                            time=unit[0][4:]
-                            revert=unit[1]
-                            version=unit[2]
-                            user=unit[3]
-                            table['time'].append(time)
-                            table['revert'].append(revert)
-                            table['version'].append(version)
-                            table['user'].append(user)
-                            table['article'].append(current)
-                            rows +=1
-                            nowat +=1
-                        elif rows>=max_row and nowat<maxline:
-                            unit=content.split(' ')
-                            time=unit[0][4:]
-                            revert=unit[1]
-                            version=unit[2]
-                            user=unit[3]
-                            table['time'].append(time)
-                            table['revert'].append(revert)
-                            table['version'].append(version)
-                            table['user'].append(user)
-                            table['article'].append(current)
-                            exceed=True
-                            rows +=1
-                            nowat +=1
                         else:
+                            if rows<max_row:
+                                unit=content.split(' ')
+                                time=unit[0][4:]
+                                revert=unit[1]
+                                version=unit[2]
+                                user=unit[3]
+                                table['time'].append(time)
+                                table['revert'].append(revert)
+                                table['version'].append(version)
+                                table['user'].append(user)
+                                table['article'].append(current)
+                                rows +=1
+                                nowat +=1
+                            else:
+                                unit=content.split(' ')
+                                time=unit[0][4:]
+                                revert=unit[1]
+                                version=unit[2]
+                                user=unit[3]
+                                table['time'].append(time)
+                                table['revert'].append(revert)
+                                table['version'].append(version)
+                                table['user'].append(user)
+                                table['article'].append(current)
+                                exceed=True
+                                rows +=1
+                                nowat +=1
+                    else:
+                        try:
+                            unit=content.split(' ')
+                            time=unit[0][4:]
+                            revert=unit[1]
+                            version=unit[2]
+                            user=unit[3]
+                            table['time'].append(time)
+                            table['revert'].append(revert)
+                            table['version'].append(version)
+                            table['user'].append(user)
+                            table['article'].append(current)
                             count +=1
                             csvname=filename+'_'+str(count)+'.csv'
                             the_df=pd.DataFrame(table)
                             cvpath=os.path.join(filedir, csvname)
                             the_df.to_csv(cvpath, index=False)
+                            print("creat 5-million-row table", count)
                             rows=0
                             table={'article':[],'time':[], 'revert':[], 'version':[], 'user':[]}
                             exceed=False
+                            break
+                        except Exception as e:
+                            count +=1
+                            csvname=filename+'_'+str(count)+'.csv'
+                            the_df=pd.DataFrame(table)
+                            cvpath=os.path.join(filedir, csvname)
+                            the_df.to_csv(cvpath, index=False)
+                            print("creat 5-million-row table", count)
+                            rows=0
+                            table={'article':[],'time':[], 'revert':[], 'version':[], 'user':[]}
+                            exceed=False
+                            break
+                            
+                        
+      
 
     return
